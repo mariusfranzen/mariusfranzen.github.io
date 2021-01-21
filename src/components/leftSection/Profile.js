cdb44018733634f0192bc1b6878e692462e84f55
Marius Franzén <marius.franzen1@gmail.com>
2021-01-21 16:01:05 +0100

    updates

--- src/components/leftSection/Profile.js
+++ src/components/leftSection/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React } from 'react';
 import ProfilePicture from '../../images/IMG_1253.jpg';
 
 function Profile() {
@@ -10,8 +10,8 @@ function Profile() {
             <h1>Marius Franzén</h1>
             <h2>Full Stack utvecklare - Jobbsökande</h2>
             <div className="row-group profile-button-group">
-                <button className="profile-button">CV som PDF</button>
-                <button className="profile-button">Kontakta mig</button>
+                <a className="profile-button" href="https://docs.google.com/document/d/1-3GkOMaMEPQoB-SBxvXDJ3l0Bh21wMsR7XPlqaGZ6Dc/edit?usp=sharing">Mitt CV på google docs</a>
+                <a className="profile-button" href="mailto:marius.franzen1@gmail.com">Kontakta mig</a>
             </div>
         </div>
     )
