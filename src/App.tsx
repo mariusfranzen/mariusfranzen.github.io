e5936e2ecdadb0fb0de13f0f69a613f84fb7d6ad
Marius Franzén <marius.franzen1@gmail.com>
2021-07-07 14:26:12 +0200

    basic setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Typography } from '@material-ui/core'
+import React, { ReactElement } from 'react'
+
+interface Props {
+
+}
+
+function App({ }: Props): ReactElement {
+	return (
+		<div>
+			<Typography>hej</Typography>
+		</div>
+	)
+}
+
+export default App
+
