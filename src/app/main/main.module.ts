976c2c922998e08d54df199a39a95b2c315b2eb8
mariusfranzen <marius.franzen1@gmail.com>
2023-05-09 11:34:06 +0200

    resume work

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,12 +4,14 @@ import { ResumeComponent } from './resume/resume.component';
 import { MenuComponent } from './menu/menu.component';
 import { MainComponent } from './main/main.component';
 import { AppCommonModule } from '../common/app-common.module';
+import { SkillMeterComponent } from './resume/components/skill-meter/skill-meter.component';
 
 @NgModule({
   declarations: [
     ResumeComponent,
     MenuComponent,
-    MainComponent
+    MainComponent,
+    SkillMeterComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/main/resume/components/skill-meter/skill-meter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-meter',
+  templateUrl: './skill-meter.component.html',
+  styleUrls: ['./skill-meter.component.scss']
+})
+export class SkillMeterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
